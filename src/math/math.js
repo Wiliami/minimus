0ebfffbbc6b232dceacfcae2715cbdce56212090
wiliami <wiliaup97@gmail.com>
2025-07-16 14:09:18 -0400

    feat: sum numbers

---
+++ src/math/math.js
@@ -0,0 +1,15 @@
+export function sum(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function mult(a, b) {
+    return a * b;
+}
+
+export function divider(a, b) {
+    return a / b;
+}
